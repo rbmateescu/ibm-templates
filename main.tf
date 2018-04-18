787797a5e9d642768170e98180f726a5a94652d6
mateescu <mateescu@ca.ibm.com>
2018-04-18 14:30:41 -0400

    single line

--- main.tf
+++ main.tf
@@ -7,10 +7,7 @@ variable "cluster_name" {
 
 resource "null_resource" "environment" {
   provisioner "local-exec" {
-    command = "bx login -a https://api.ng.bluemix.net --apikey $BLUEMIX_API_KEY && \
-    bx plugin install -f container-service && \
-    bx cs cluster-config --export $BX_CLUSTER_NAME && \ 
-    helm init"
+    command = "bx login -a https://api.ng.bluemix.net --apikey $BLUEMIX_API_KEY && bx plugin install -f container-service && bx cs cluster-config --export $BX_CLUSTER_NAME && helm init"
     environment {
       BX_CLUSTER_NAME = "${cluster_name}"
     }
