5e319d1c527757bbe8a1d16e2efc47da59336572
mateescu <mateescu@ca.ibm.com>
2018-04-18 13:14:56 -0400

    bx plugin

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
 provider "ibm" {
 }
 
+variable "cluster_name" {
+  description = "The base name for the cluster."
+}
+
 resource "null_resource" "environment" {
   provisioner "local-exec" {
-    command = "echo $BLUEMIX_API_KEY"
+    command = "bx login -a https://api.ng.bluemix.net --apikey $BLUEMIX_API_KEY && \
+    bx plugin install -f container-service && \
+    bx cs cluster-config --export $BX_CLUSTER_NAME $$ \ 
+    helm init "
+    environment {
+      BX_CLUSTER_NAME = "${cluster_name}"
+    }
   }
+
+
 }
